dc301f42573d734b48a43869f6666eb0bc30db82
sen.meng <sen.meng@cootek.cn>
2020-03-04 20:40:40 +0800

    update 更新icon字体文件

--- src/components/GobackNavbar/index.jsx
+++ src/components/GobackNavbar/index.jsx
@@ -4,7 +4,7 @@ import Style from './index.module.css';
 export default function GobackNavbar() {
 	return (
 		<div className={Style.wrapper}>
-			<i className={Style.back}></i>
+			<i className={`${Style.back} flex icon`}></i>
 		</div>
 	)
 }
